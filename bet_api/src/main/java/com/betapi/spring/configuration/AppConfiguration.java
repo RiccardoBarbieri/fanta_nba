98783a83ed300f0e633f9bf3ce18fc45088ff4d9
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-05-30 17:49:50 +0200

    Configuration for mongodb on azure

---
+++ bet_api/src/main/java/com/betapi/spring/configuration/AppConfiguration.java
@@ -0,0 +1,12 @@
+package com.betapi.spring.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableMongoRepositories(basePackages = "com.betapi.database.repositories")
+public class AppConfiguration {
+}
