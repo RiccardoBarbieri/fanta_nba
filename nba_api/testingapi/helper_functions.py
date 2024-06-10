5f6a1304f43cbea5ace6c8419f5690bf499a3afe
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-06-10 20:37:55 +0200

    Created some functions to join two game dicts together into one GameInfo, check info for details

---
+++ nba_api/testingapi/helper_functions.py
@@ -0,0 +1,83 @@
+from typing import List, AnyStr, Dict
+
+# Static variables, to put in a CONSTANTS module
+# Relevant fields from LeagueGameFinder dictionary
+LEAGUE_GAME_FINDER_FIELDS = [
+    'team_id', 'fgm', 'fga', 'fg3m', 'fg3a', 'ftm', 'fta', 'oreb', 'dreb', 'reb', 'ast', 'stl', 'blk', 'tov',
+    'pf', 'pts'
+]
+
+
+# Helper functions
+
+
+def get_home_away_team(matchups: AnyStr) -> Dict[AnyStr, AnyStr]:
+    """
+    Get the home and away teams from a matchup string.
+
+    :param matchups: A string representing a matchup.
+    :return: A dictionary containing the home and away teams (home_team and away_team keys).
+    """
+    if ' vs. ' in matchups:
+        home_team, away_team = matchups.split(' vs. ')
+    elif ' @ ' in matchups:
+        away_team, home_team = matchups.split(' @ ')
+    else:
+        raise ValueError('Matchup string must contain either " vs. " or " @ "')
+    return {'home_team': home_team, 'away_team': away_team}
+
+
+def all_keys_to_lower(d: Dict | List[Dict]) -> Dict | List[Dict]:
+    """
+    Convert all keys in a dictionary or list of dictionaries to lowercase.
+
+    :param d: A dictionary or list of dictionaries.
+    :return: A dictionary or list of dictionaries with all keys in uppercase.
+    """
+    if isinstance(d, list) and d and isinstance(d[0], dict):
+        return [all_keys_to_lower(i) for i in d]
+    elif isinstance(d, dict):
+        return {k.lower(): v for k, v in d.items()}
+
+
+# Main functions
+
+
+def join_to_game_info(game1: Dict, game2: Dict) -> Dict:
+    """
+    Join two game stats dictionaries about the same game, resulting dict will have all relevant
+     stats preceded by home_ and away_.
+    The function expects two dict gathered from the nba_api.stats.endpoints.teamgamelog.TeamGameLog endpoint.
+
+    :param game1: A dictionary representing the stats of a game.
+    :param game2: A dictionary representing the stats of a game.
+    :return: A dictionary containing the stats of both games.
+    """
+    game1 = all_keys_to_lower(game1)
+    game2 = all_keys_to_lower(game2)
+
+    matchup = game1['matchup']
+    home_team, away_team = get_home_away_team(matchup).values()
+
+    # Differentiating between home and away dictionaries
+    home_dict = game1 if game1['matchup'].startswith(home_team) else game2
+    away_dict = game1 if game1['matchup'].startswith(away_team) else game2
+
+    # Setting generale game info
+    game_info = {}
+    game_info['id'] = home_dict['game_id']
+    game_info['date'] = home_dict['game_date']
+    game_info['home_team_id'] = home_dict['team_id']
+    game_info['away_team_id'] = away_dict['team_id']
+    game_info['winner'] = home_team if home_dict['wl'] == 'W' else away_team
+    game_info['home_wins_until_game'] = home_dict['w']
+    game_info['home_losses_until_game'] = home_dict['l']
+    game_info['away_wins_until_game'] = away_dict['w']
+    game_info['away_losses_until_game'] = away_dict['l']
+
+    # Setting home and away stats
+    for field in LEAGUE_GAME_FINDER_FIELDS:
+        game_info[f"home_{field}"] = home_dict[field]
+        game_info[f"away_{field}"] = away_dict[field]
+
+    return game_info
---
+++ nba_api/testingapi/storic-game-stats_methodtest.py
@@ -0,0 +1,75 @@
+from nba_api.stats.endpoints.teamgamelog import TeamGameLog
+import nba_api.stats.static.teams as teams
+from helper_functions import join_to_game_info, all_keys_to_lower
+import json
+import re
+import datetime
+from pprint import pprint
+
+# Endpoint parameters:
+# - team1_ticker (string) the team abbreviation
+# - team2_ticker (string) the team abbreviation
+# - fromdate (date string) ISO 8601 (YYYY/MM/DD) to convert to MM/DD/YYYY to use with nba-api
+# - todate (date string) ISO 8601 (YYYY/MM/DD) to convert to MM/DD/YYYY to use with nba-api
+
+# Endpoint parameters
+dateFrom = '2024-01-01'
+dateTo = '2024-06-06'
+teamTicker1 = 'BOS'
+teamTicker2 = 'WAS'
+
+teams = teams.get_teams()
+
+ticker1_id = filter(lambda x: x['abbreviation'] == teamTicker1, teams).__next__()['id']
+ticker2_id = filter(lambda x: x['abbreviation'] == teamTicker2, teams).__next__()['id']
+
+# convert to MM/DD/YYYY using datetime
+date_from = datetime.datetime.strptime(dateFrom, '%Y-%m-%d').strftime('%m/%d/%Y')
+date_to = datetime.datetime.strptime(dateTo, '%Y-%m-%d').strftime('%m/%d/%Y')
+
+# Getting Boston Celtics games
+ticker1_game_log = TeamGameLog(team_id=ticker1_id,
+                               date_to_nullable=date_to,
+                               date_from_nullable=date_from
+                               ).get_normalized_dict()['TeamGameLog']
+
+# Getting Washington Wizards games
+ticker2_game_log = TeamGameLog(team_id=ticker2_id,
+                               date_to_nullable=date_to,
+                               date_from_nullable=date_from
+                               ).get_normalized_dict()['TeamGameLog']
+
+ticker1_game_log = all_keys_to_lower(ticker1_game_log)
+ticker2_game_log = all_keys_to_lower(ticker2_game_log)
+
+# (BOS|WAS) (vs.|@) (BOS|WAS)
+re.compile(rf'({teamTicker1}|{teamTicker2}) (vs.|@) ({teamTicker1}|{teamTicker2})')
+
+ticker1_games = list(
+    filter(lambda x: re.match(rf'{teamTicker1} (vs.|@) {teamTicker2}', x['matchup']), ticker1_game_log))
+ticker2_games = list(
+    filter(lambda x: re.match(rf'{teamTicker2} (vs.|@) {teamTicker1}', x['matchup']), ticker2_game_log))
+
+# Sanity checks, both lists should have the same length and same game_ids
+if len(ticker1_games) != len(ticker2_games):
+    raise ValueError('Both lists should have the same amount of games')
+
+if set([i['game_id'] for i in ticker1_games]) != set([i['game_id'] for i in ticker2_games]):
+    raise ValueError('Both lists should have the same games')
+
+# Sorting both lists by game_id to join them correctly
+ticker1_games = sorted(ticker1_games, key=lambda x: x['game_id'])
+ticker2_games = sorted(ticker2_games, key=lambda x: x['game_id'])
+
+with open('game_log_ticker1.json', 'w+') as f:
+    json.dump(ticker1_games, f, indent=4)
+
+with open('game_log_ticker2.json', 'w+') as f:
+    json.dump(ticker2_games, f, indent=4)
+
+# Joining games
+joined_games = [join_to_game_info(ticker1_games[i], ticker2_games[i]) for i in range(len(ticker1_games))]
+
+with open('joined_games.json', 'w+') as f:
+    json.dump(joined_games, f, indent=4)
+
