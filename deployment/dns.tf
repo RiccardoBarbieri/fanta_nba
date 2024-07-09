bf01e34a18f8eb9b2039498a13d47026f9e4352d
RiccardoBarbieri <riccardo_barbieri@outlook.it>
2024-07-09 11:58:08 +0200

    Cleaned tf scripts

--- deployment/dns.tf
+++ deployment/dns.tf
@@ -1,20 +1,3 @@
-
-
-# locals {
-#   input_string = "hello-world--1k7gmlj.kindgrass-b69cf8c6.westeurope.azurecontainerapps.io"
-# #   input_string = azurerm_container_app.container.latest_revision_fqdn
-#   before_rev = split("--", local.input_string)[0]
-#   after_rev = index(local.input_string, ".")
-# #   after_rev = slice(split(".", local.input_string), 1, length(split(".", local.input_string)) - 1)
-#
-#   fqdn = join(".", [local.before_rev, local.after_rev])
-#
-# }
-
-# output "fqdn" {
-#   value = local.fqdn
-# }
-
 data "azurerm_dns_zone" "autoboost" {
   name                = "autoboost.it"
   resource_group_name = data.azurerm_resource_group.main_group.name
