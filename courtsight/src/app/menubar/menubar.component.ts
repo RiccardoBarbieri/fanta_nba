5c2bdaac37f4d41985cb734eefa6214245ad2259
Leonardo Baraldi <leobaraldi2000@gmail.com>
2024-07-10 18:40:15 +0200

    Added webapp with homepage

---
+++ courtsight/src/app/menubar/menubar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menubar',
+  standalone: true,
+  imports: [],
+  templateUrl: './menubar.component.html',
+  styleUrl: './menubar.component.css'
+})
+export class MenubarComponent {
+
+}
