5c19700580577e9a222c6e6c6442316f4c582ad7
Leonardo Ruberto <leonardo.ruberto@studio.unibo.it>
2024-07-15 17:13:27 +0200

    FIX commenceTimeFrom/To parameter format

--- bet_api/src/main/java/betapi/oddsapiservice/client/ApiKeyInterceptor.java
+++ bet_api/src/main/java/betapi/oddsapiservice/client/ApiKeyInterceptor.java
@@ -1,5 +1,6 @@
 package betapi.oddsapiservice.client;
 
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
@@ -23,13 +24,30 @@ public class ApiKeyInterceptor implements ClientHttpRequestInterceptor {
         this.apiKey = apiKey;
     }
 
+    @NotNull
     @Override
-    public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+    public ClientHttpResponse intercept(HttpRequest request, @NotNull byte[] body, @NotNull ClientHttpRequestExecution execution) throws IOException {
         log.debug("Request: {} {} - adding api key", request.getMethod(), request.getURI());
         //modify request uri adding parameter
         UriBuilder uriBuilder = UriComponentsBuilder.fromUri(request.getURI());
         uriBuilder.queryParam("apiKey", apiKey);
+
+        // Check and modify commenceTimeFrom if present
+        String commenceTimeFrom = request.getURI().getQuery(); // Get the original query string
+        if (commenceTimeFrom != null && commenceTimeFrom.contains("commenceTimeFrom")) {
+            commenceTimeFrom = commenceTimeFrom.split("=")[1].split("Z")[0].replace("%3A", ":")+"Z";
+            uriBuilder.replaceQueryParam("commenceTimeFrom", commenceTimeFrom);
+        }
+
+        // Check and modify commenceTimeTo if present
+        String commenceTimeTo = request.getURI().getQuery(); // Get the original query string
+        if (commenceTimeTo != null && commenceTimeTo.contains("commenceTimeTo")) {
+            commenceTimeTo = commenceTimeTo.split("commenceTimeTo=")[1].split("Z")[0].replace("%3A", ":")+"Z";
+            uriBuilder.replaceQueryParam("commenceTimeTo", commenceTimeTo);
+        }
+
         HttpRequest modifiedRequest = new HttpRequestWrapper(request) {
+            @NotNull
             @Override
             public URI getURI() {
                 return uriBuilder.build();
